2a886b8c4fbe21c032d004c0ce39faa34c860fd0
Nizomov <48754111+nizomovasadbek@users.noreply.github.com>
2025-09-11 15:43:50 +0500

    Dummy data send

--- Core/Src/tasks/tx_command.c
+++ Core/Src/tasks/tx_command.c
@@ -29,6 +29,15 @@ void txcommand_poll(  void  ) {
 	while(  1  ) {
 
 		cmd.M1  =  (  u8  )  map(  adc.M1, 1258, 3480, 0, 255  );
+		cmd.M2 = (  u8  ) 127;
+		cmd.M3 = (  u8  ) 127;
+		cmd.M4 = (  u8  ) 127;
+		cmd.M5 = (  u8  ) 127;
+		cmd.M6 = (  u8  ) 127;
+		cmd.M7 = (  u8  ) 127;
+		cmd.M8 = (  u8  ) 127;
+		cmd.M9 = (  u8  ) 127;
+
 		printf(  "Motor PWM: %d\r\n", cmd.M1  );
 
 		//TODO: map and send all sticks
